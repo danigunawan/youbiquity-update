9cfb7f75d886bed57fd5c5ad17989931df25560b
Randy Jap <randyjap@m.local>
2018-08-26 19:06:18 +0800

    improve queries

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -3,52 +3,112 @@
 module Api
   class SearchController < ApplicationController
     def index
-      brand_filters = brand_params.empty? ? Brand.all.pluck(:name) : brand_params
-      category_filters = category_params.empty? ? Category.all.pluck(:name) : category_params
-      rating_filter = rating_params.empty? ? 0 : rating_params.to_i - 0.5
-      price_filter = price_params.empty? ? 99_999 : price_params
-      bounds_filter = bounds_params.empty? ? nil : bounds_params
+      hash_return = {}
+      listings.each do |listing|
+        hash_return[listing["id_of_listing"]] = {
+          id: listing["id_of_listing"],
+          lessor: listing["lessor"],
+          brand: listing["brand"],
+          category: listing["category"],
+          day_rate: listing["day_rate"],
+          lat: listing["lat"],
+          lng: listing["lng"],
+          rating_average: listing["review_average"].to_f.round(1),
+          review_count: listing["review_count"],
+          photos: photos[listing["id_of_listing"]].map { |photo| { image_url: photo.image_url, photo_id: photo.id } },
+        }
+      end
+      render json: hash_return, status: 200
+    end
 
-      @listings = Listing.
-        joins(:category, :brand, :rentals, :reviews, :photos).
-        where(categories: { name: category_filters }).
-        where(brands: { name: brand_filters }).
-        where(active: true).
-        where("day_rate <= ?", price_filter).
-        group("listings.id").
-        having("AVG(reviews.review) >= ?", rating_filter)
-
-      @listings =
-        if bounds_filter && bounds_filter[:southWest][:lng].to_f < bounds_filter[:northEast][:lng].to_f
-          @listings.
-            where("lat BETWEEN ? AND ?", bounds_filter[:southWest][:lat], bounds_filter[:northEast][:lat])
-        elsif bounds_filter
-          @listings.
-            where("lat BETWEEN ? AND ?", bounds_filter[:southWest][:lat], bounds_filter[:northEast][:lat]).
-            where("lng > ? OR lng < ?", bounds_filter[:southWest][:lng], bounds_filter[:northEast][:lng])
-        end
+    def bounds_wrap_around?
+      bounds_filter && bounds_filter[:southWest][:lng].to_f < bounds_filter[:northEast][:lng].to_f
     end
 
     private
 
-    def brand_params
-      params[:brand]
+    def brand_filters
+      params[:brand].map { |b| "'#{b}'" } unless params[:brand].blank?
     end
 
-    def category_params
-      params[:category]
+    def category_filters
+      params[:category].map { |c| "'#{c}'" } unless params[:category].blank?
     end
 
-    def rating_params
-      params[:rating]
+    def price_filter
+      params[:price] unless params[:price].blank?
     end
 
-    def bounds_params
-      params[:bounds]
+    def rating_filter
+      params[:rating].to_i - 0.5 unless params[:rating].blank?
     end
 
-    def price_params
-      params[:price]
+    def bounds_filter
+      params[:bounds] unless params[:bounds].blank?
+    end
+
+    def sql_joins
+      "SELECT
+        listings.id AS id_of_listing,
+        users.username AS lessor,
+        brands.name AS brand,
+        categories.name AS category,
+        listings.day_rate,
+        listings.lat,
+        listings.lng,
+        listings.listing_title,
+        AVG(reviews.review) AS review_average,
+        COUNT(reviews.review) AS review_count
+      FROM
+        listings
+      JOIN users ON users.id = listings.lessor_id
+      JOIN brands ON brands.id = listings.brand_id
+      JOIN categories ON categories.id = listings.category_id
+      JOIN rentals ON rentals.listing_id = listings.id
+      JOIN reviews ON reviews.rental_id = rentals.id"
+    end
+
+    def sql_wheres
+      sql_statement = "WHERE listings.active = TRUE"
+      sql_statement += " AND brands.name IN (#{brand_filters.join(',')})" if brand_filters
+      sql_statement += " AND categories.name IN (#{category_filters.join(',')})" if category_filters
+      sql_statement += " AND listings.day_rate <= #{price_filter}" if price_filter
+
+      if bounds_wrap_around?
+        sql_statement +=
+          " AND listings.lat > #{bounds_filter[:southWest][:lat]}" \
+          " AND listings.lat < #{bounds_filter[:northEast][:lat]}"
+      elsif bounds_filter
+        sql_statement +=
+          " AND listings.lat > #{bounds_filter[:southWest][:lat]}" \
+          " AND listings.lat < #{bounds_filter[:northEast][:lat]}" \
+          " AND listings.lng > #{bounds_filter[:southWest][:lng]}" \
+          " OR listings.lng < #{bounds_filter[:northEast][:lng]}"
+      end
+      sql_statement
+    end
+
+    def sql_group_bys
+      sql_statement = "GROUP BY
+        listings.id,
+        lessor,
+        brand,
+        category"
+      sql_statement += " HAVING AVG(reviews.review) >= #{rating_filter}" if rating_filter
+      sql_statement
+    end
+
+    def listings
+      @listings ||= ActiveRecord::Base.
+        connection.
+        execute([sql_joins, sql_wheres, sql_group_bys].
+        join("\n"))
+    end
+
+    def photos
+      @photos ||= Photo.
+        where(listing_id: listings.pluck("id_of_listing")).
+        each_with_object(Hash.new { |h, k| h[k] = [] }) { |photo, hash| hash[photo.listing_id] << photo }
     end
   end
 end
