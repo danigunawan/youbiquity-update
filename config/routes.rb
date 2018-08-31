f9f1b7786af9122ad578d37870fb1abd0cd0e815
Randy Jap <randyjap@m.local>
2018-08-31 21:43:31 +0800

    code style

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: %i[create index]
-    resource :session, only: %i[new create destroy]
-    resources :listings, only: %i[index show update create]
-    resources :rentals, only: %i[index create]
-    resources :search, only: [:index]
-    resources :options, only: [:index]
-    resources :reviews, only: [:create]
+    resources :users,     only: %i[create index]
+    resource  :session,   only: %i[new create destroy]
+    resources :listings,  only: %i[index show update create]
+    resources :rentals,   only: %i[index create]
+    resources :search,    only: %i[index]
+    resources :options,   only: %i[index]
+    resources :reviews,   only: %i[create]
   end
 end
