e5e70db575c7df4c692d4368bb7e247ebe1861f2
Randy Jap <randyjap@m.local>
2018-08-31 20:57:04 +0800

    code style

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   subject { build :brand }
+
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
   it { should have_many(:listings) }
