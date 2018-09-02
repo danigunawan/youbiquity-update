c80abe0d158e89195c6df6506cc4653aa2ec1279
Randy Jap <randyjap@gmail.com>
2018-09-02 22:07:54 +0800

    add filter specs

--- spec/controllers/api/search_controller_spec.rb
+++ spec/controllers/api/search_controller_spec.rb
@@ -33,10 +33,44 @@ RSpec.describe Api::SearchController, type: :controller do
     context "logged in" do
       before { get(action, params: params, session: { session_token: current_user.session_token }) }
 
-      it "returns the correct data format" do
-        expect(return_content.keys).to match_array(Listing.pluck(:id).map(&:to_s))
-        return_content.values.flatten.each do |listing_hash|
-          expect(listing_hash.keys).to match_array(expected_listing_return_keys)
+      context "no filters" do
+        it "returns the correct data format" do
+          expect(return_content.keys).to match_array(Listing.pluck(:id).map(&:to_s))
+          return_content.values.each do |listing_hash|
+            expect(listing_hash.keys).to match_array(expected_listing_return_keys)
+          end
+        end
+      end
+
+      context "with brand filter" do
+        let(:brand) { [current_user.listings.first.brand.name] }
+
+        it "returns the correct brand data subset" do
+          return_content.values.flatten.each do |listing_hash|
+            expect(brand).to include(listing_hash["brand"])
+          end
+        end
+      end
+
+      context "with category" do
+        let(:category) { [current_user.listings.first.category.name] }
+
+        it "returns the correct category data subset" do
+          return_content.values.flatten.each do |listing_hash|
+            expect(category).to include(listing_hash["category"])
+          end
+        end
+      end
+
+      context "with day_rate filter" do
+        let(:price) { 2 }
+
+        it "returns the correct day_rate data subset" do
+          create :listing, day_rate: 1
+
+          return_content.values.flatten.each do |listing_hash|
+            expect(listing_hash["day_rate"]).to be < price
+          end
         end
       end
     end
