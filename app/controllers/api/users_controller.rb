1e581c36c2ad9dc4c629f06bfc60b8a780abcd93
Randy Jap <randyjap@gmail.com>
2018-09-02 12:32:20 +0800

    remove unused action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,10 +4,6 @@ module Api
   class UsersController < ApplicationController
     skip_before_action :require_login
 
-    def new
-      @user = User.new
-    end
-
     def create
       @user = User.new(user_params)
       if @user.save
